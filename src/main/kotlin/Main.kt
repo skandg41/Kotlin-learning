3884896c1fb00ffe0e0e124969342447c02fcd80
Skand Gupta <skandg41@gmail.com>
2023-09-23 18:49:13 +0530

    First commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+//fun main(args: Array<String>) {
+//    println("Hello World!")
+//
+//    // Try adding program arguments via Run/Debug configuration.
+//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
+//    print(5243)
+//
+//}
+fun main() {
+    var ourFirstVariable: Boolean = true
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println(ourFirstVariable)
 
+    ourFirstVariable = false
+    println(ourFirstVariable)
 }
