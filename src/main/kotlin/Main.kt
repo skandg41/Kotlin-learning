76ba687a0a7feded6d505d16ba8b770d7be2b9b5
Skand Gupta <skandg41@gmail.com>
2023-09-24 14:12:37 +0530

    String data type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,46 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val aChar:Char ='a'
+    val aNumericChar = '1'
+    val sNewLineChar = '\n'
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val aString:String = "Hello Kotlin"
+    val escaped = "Hello \n World"
+    println(escaped)
 
+    val row = """
+        Some
+             multiline
+        text
+        is going on
+        here
+    """.trimIndent()
+    println(row)
+    val row2 = """
+        >>Some
+             multiline
+        text
+        >>is going on
+        here
+    """.trimMargin(">>")
+    print(row2)
+
+    val concatenateString = "Hello" + "Kotlin"
+
+    val concatNumber = " The Number " + 16 + "\n"     // Will concate two strings and form a new string
+    println(concatNumber)
+
+    var stringdk = "sfdsfd\n"
+    println(stringdk)
+
+    val aNumber = 16
+    val concatTemplate = "The number ${1000 + aNumber} $aChar"      // Will be a single string
+    println(concatTemplate)
+
+    println(stringdk.isEmpty())
+
+    val someString = "   "
+    println(someString.isBlank())
+
+    val anotherString = "Hello Kotlin Workd"
+    println(anotherString.contains("Kotlin"))
 }
