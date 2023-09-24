138327e8bae9951ec6232341a83339dc583d9d42
Skand Gupta <skandg41@gmail.com>
2023-09-24 13:12:48 +0530

    data types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,6 +12,6 @@ fun main() {
 
     println(ourFirstVariable)
 
-    ourFirstVariable = false
-    println(ourFirstVariable)
+    println(ourFirstVariable == true)
+    println(ourFirstVariable == false)
 }
